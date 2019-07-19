41ad826019543e473166203b7ec86fa88961a4a2
Sundayfree <978591397@qq.com>
2019-07-19 19:27:32 +1200

    user service completed

---
+++ src/main/java/com/weltec/grocery/service/IUserService.java
@@ -0,0 +1,18 @@
+package com.weltec.grocery.service;
+
+import com.weltec.grocery.pojo.User;
+
+import java.util.List;
+
+/**
+ * Description:
+ * Author: Liu
+ * Date: 2019-07-19 19:18
+ */
+public interface IUserService {
+   List<User> findAll();
+   void deleteUser(String id);
+   void updateUser(User u);
+   void createUser(User u);
+
+}
---
+++ src/main/java/com/weltec/grocery/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.weltec.grocery.service;
+
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+import com.weltec.grocery.dao.IUserRepository;
+import com.weltec.grocery.pojo.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Description:
+ * Author: Liu
+ * Date: 2019-07-19 19:22
+ */
+public class UserServiceImpl implements IUserService{
+
+    @Autowired
+    IUserRepository userRepository;
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll ();
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        userRepository.deleteById (id);
+    }
+
+    @Override
+    public void updateUser(User u) {
+        User user = userRepository.findById (u.getUserId ()).get ();
+        BeanUtils.copyProperties (u,user);
+    }
+
+    @Override
+    public void createUser(User u) {
+        userRepository.save (u);
+    }
+}
