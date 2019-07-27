b97bdad2640791f9cccab6cf217f2f2050e3f9ed
Sundayfree <978591397@qq.com>
2019-07-27 19:46:39 +1200

    done

--- src/test/java/com/weltec/grocery/GroceryApplicationTests.java
+++ src/test/java/com/weltec/grocery/GroceryApplicationTests.java
@@ -4,9 +4,11 @@ import com.weltec.grocery.dao.IProductRepository;
 import com.weltec.grocery.dao.IUserRepository;
 import com.weltec.grocery.pojo.Product;
 import com.weltec.grocery.pojo.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,19 +25,19 @@ public class GroceryApplicationTests {
     IProductRepository productRepository;
 
     @Test
-    public void contextLoads() {
+    public void test() {
         List<User> all = repository.findAll ();
-        System.out.println (all.get (0)+"dddddddddddddddddd");
+        Assert.assertNotNull (all.get (0));
     }
     @Test
     public void productTest(){
         Product product = productRepository.findById ("1").get ();
-        System.out.println (product);
+        Assert.assertNotNull (product);
     }
     @Test
     public void userTest(){
         User u  = repository.findByEmail ("2");
-        System.out.println (u +"ffffffffff");
+        Assert.assertEquals ("liu",u.getUserName ());
     }
 
 }
